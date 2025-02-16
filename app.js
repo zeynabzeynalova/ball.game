5ff58287b8146c74fff7b2cc5f2417b126636324
zeynab <zzeynab694@gmail.com>
2025-02-16 13:31:46 +0400

    ahahahahahah

--- app.js
+++ app.js
@@ -49,76 +49,8 @@ function moveBall() {
     }
 }
 
-function moveObstacles() {
-    for (let i = 0; i < obstacles.length; i++) {
+function moveObstacles() 
+    for (let i = 0; i < obstacles.length; i++) 
         obstacles[i].y += obstacleSpeed;
 
-        // Engel ekranın dışına çıkarsa tekrar üstten eklenir
-        if (obstacles[i].y > canvasHeight) {
-            obstacles[i].y = -obstacleHeight;
-            obstacles[i].x = Math.random() * (canvasWidth - obstacleWidth);
-            score++;
-        }
-
-        // Engel ile topun çarpışmasını kontrol et
-        if (
-            ballX > obstacles[i].x &&
-            ballX < obstacles[i].x + obstacleWidth &&
-            ballY - ballRadius < obstacles[i].y + obstacleHeight
-        ) {
-            gameOver = true;
-        }
-    }
-}
-
-function generateObstacles() {
-    if (Math.random() < 0.02) {  // %2 ihtimalle yeni engel ekle
-        obstacles.push({
-            x: Math.random() * (canvasWidth - obstacleWidth),
-            y: -obstacleHeight
-        });
-    }
-}
-
-function drawScore() {
-    ctx.font = "16px Arial";
-    ctx.fillStyle = "#333";
-    ctx.fillText("Skor: " + score, 8, 20);
-}
-
-function drawGameOver() {
-    ctx.font = "30px Arial";
-    ctx.fillStyle = "#FF0000";
-    ctx.fillText("Oyun Bitti!", canvasWidth / 2 - 100, canvasHeight / 2);
-}
-
-function updateGame() {
-    if (gameOver) {
-        drawGameOver();
-        return;
-    }
-
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight); // Ekranı temizle
-    drawBall();
-    drawObstacles();
-    drawScore();
-    moveBall();
-    moveObstacles();
-    generateObstacles();
-    requestAnimationFrame(updateGame);
-}
-
-// Klavye ile hareket
-document.addEventListener("keydown", function (event) {
-    if (event.key === "ArrowLeft") {
-        ballSpeedX = -5;
-    } else if (event.key === "ArrowRight") {
-        ballSpeedX = 5;
-    }
-});
-
-document.addEventListener("keyup", function () {
-    ballSpeedX = 0;
-});
-
-updateGame();
+        // Engel ekranın dışına çıkarsa tekrar üstten
